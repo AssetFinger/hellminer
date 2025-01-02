f23ebea4bc45e6de28ab91d2071f9f3780271601
AssetFinger <assetfinger@gmail.com>
2025-01-02 22:30:03 +0700

    Update run_miner.sh

--- run_miner.sh
+++ run_miner.sh
@@ -4,4 +4,5 @@
 # Example SSL URL
 # ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
 
-./hellminer -c stratum+tcp://na.luckpool.net:3960 -u ADDRESS.WORKER -p x --cpu 1
+# ./hellminer -c stratum+tcp://na.luckpool.net:3960 -u ADDRESS.WORKER -p x --cpu 1
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RLb9NTSrsBMPy8tDcbT7EvCMFgjSiL85u4.work -p x --cpu 1
